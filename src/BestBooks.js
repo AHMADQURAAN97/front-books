2c9679f68dfd8055792b1787068eda4d5e075b0c
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-09-05 18:00:49 +0300

    sdx

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,31 +1,18 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
-class BestBooks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    }
-  }
-
-  /* TODO: Make a GET request to your API to fetch books for the logged in user  */
-
+class MyFavoriteBooks extends React.Component {
   render() {
-
-    /* TODO: render user's books in a Carousel */
-
-    return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
+    return(
+      <Jumbotron>
+        <h1>My Favorite Books</h1>
+        <p>
+          This is a collection of my favorite books
+        </p>
+      </Jumbotron>
     )
   }
 }
 
-export default BestBooks;
+export default MyFavoriteBooks;
